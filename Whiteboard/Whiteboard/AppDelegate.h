4e6c2fe1f2d5bc391f24b9598726c4b441e254ee
briguy <briansinnicke@gmail.com>
2015-07-24 15:33:30 -0400

    Initial Commit

---
+++ Whiteboard/Whiteboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Whiteboard
+//
+//  Created by Brian Sinnicke on 7/24/15.
+//  Copyright (c) 2015 Brian Sinnicke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Whiteboard/Whiteboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Whiteboard
+//
+//  Created by Brian Sinnicke on 7/24/15.
+//  Copyright (c) 2015 Brian Sinnicke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
