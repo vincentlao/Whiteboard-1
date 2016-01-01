cf548e6c36a33e8e5acc067f0b21a1968dce2b70
briguy <briansinnicke@gmail.com>
2016-01-01 15:01:41 -0500

    Add isDeleted WIP restructure and make DrawingData more centralized

--- Whiteboard/Whiteboard/DrawingData.h
+++ Whiteboard/Whiteboard/DrawingData.h
@@ -7,13 +7,19 @@
 //
 
 #import <Realm/Realm.h>
+#import "PlaybackData.h"
+#import "DrawLinesData.h"
 
 @interface DrawingData : RLMObject
 @property (nonatomic, readwrite) NSInteger draw_id;
 @property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) NSString *author;
 @property (nonatomic, strong) NSData *imageData;
+@property (nonatomic, assign) BOOL isDeleted;
 @property (nonatomic, strong) NSDate *createdAt;
+@property (nonatomic, strong) NSDate *updatedAt;
+@property (nonatomic, strong) RLMArray<PlaybackData *> *playback;
+@property (nonatomic, strong) RLMArray<DrawLinesData *> *drawlines;
 @end
 
 // This protocol enables typed collections. i.e.:
