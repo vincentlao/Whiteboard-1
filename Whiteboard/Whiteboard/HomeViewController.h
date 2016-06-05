9a7b3fe892073336e4232a23b1e94ec701ccc09f
devbriguy <briansinnicke@gmail.com>
2016-06-04 22:18:40 -0400

    Remove cocoa pod integration and Realm pod; Add HomeViewController

---
+++ Whiteboard/Whiteboard/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Whiteboard
+//
+//  Created by Brian Sinnicke on 5/21/16.
+//  Copyright © 2016 Brian Sinnicke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
